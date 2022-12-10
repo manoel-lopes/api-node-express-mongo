2c25679b3a2294d1338050c0bb8233a51a6082dc
manoel-lopes <manoafonso.lopes@hotmail.com>
2022-12-10 13:27:29 -0300

    Adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import { routes } from 'routes'
 
 const PORT = process.env.PORT || 3000
@@ -9,6 +10,7 @@ const MONGO_URI = process.env.MONGO_URI as string
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 mongoose.connect(MONGO_URI)
 
